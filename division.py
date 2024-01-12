31baf65c371cfd1c684d2404a84af4d694f4842a
wiyiyi <150380794+wiyiyi@users.noreply.github.com>
2024-01-12 14:50:53 -0600

    Update division.py

---
+++ division.py
@@ -0,0 +1,12 @@
+def divi ():
+    n=int (input ("numero 1:"))
+    n1=int (input ("numero2:"))
+    r=n/n1
+    print (r)
+divi()
+
+def divi1 (n,n1):
+    divi2=n/n1
+    print (divi2)
+
+divi1(10,2)
