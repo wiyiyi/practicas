a774a4fc7983707d9edaea8ee8b7aa125dffc9aa
wiyiyi <150380794+wiyiyi@users.noreply.github.com>
2024-01-26 13:23:08 -0600

    multiplicacion1.py

--- multiplicacion1.py
+++ multiplicacion1.py
@@ -1,6 +1,13 @@
-def multiplicaion ():
+def multi ():
      n=int (input("dame numero 1:")) 
      n1=int (input("dame numero 2:")) 
      r=n*n1
      print (r)
-multiplicaion()
+
+
+def multie (n,n1): 
+    mult=n*n1
+    print (mult)
+
+multi()
+multie(5,2)
